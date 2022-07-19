348c2f26d8e1115facd8358248b7cd6f9ffc6119
Ranjit Kolkar <ranjit.kolkar@gmail.com>
2022-07-19 11:36:22 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,12 @@
-<?php include('header.php') 
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+</head>
+<body>
 
-
-?>
 <?php
 $query = @unserialize (file_get_contents('http://ip-api.com/php/'));
 if ($query && $query['status'] == 'success') {
@@ -11,3 +16,6 @@ foreach ($query as $data) {
     echo $data . "<br>";
 }
 ?>
+
+</body>
+</html>
